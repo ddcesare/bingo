6ca4f3568ea05e51b3e2e4a8c8ce314bbca6a7f3
dd di cesare <didi@posteo.net>
2017-09-18 23:52:49 +0200

    [basics] adding types

--- Bingo.elm
+++ Bingo.elm
@@ -4,10 +4,12 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+playerInfo : String -> Int -> String
 playerInfo name gameNumber =
     name ++ " - Game #" ++ toString gameNumber
 
 
+viewPlayer : String -> Int -> Html msg
 viewPlayer name gameNumber =
     let
         playerInfoText =
@@ -18,11 +20,13 @@ viewPlayer name gameNumber =
     h2 [ id "info", class "classy" ] [ playerInfoText ]
 
 
+viewHeader : String -> Html msg
 viewHeader title =
     header []
         [ h1 [] [ text title ] ]
 
 
+viewFooter : Html msg
 viewFooter =
     footer []
         [ a [ href "http://elm-lang.org" ]
@@ -30,6 +34,7 @@ viewFooter =
         ]
 
 
+view : Html msg
 view =
     div [ class "content" ]
         [ viewHeader "BUZZWORD BINGO"
@@ -38,5 +43,6 @@ view =
         ]
 
 
+main : Html msg
 main =
     view
