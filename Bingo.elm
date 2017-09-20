cb1e5a7898662b84826ac3823ce6968f0a3dd1da
dd di cesare <didi@posteo.net>
2017-09-20 15:51:12 +0200

    [model] initial model

--- Bingo.elm
+++ Bingo.elm
@@ -4,6 +4,26 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+-- MODEL
+
+
+initialModel =
+    { name = "dd"
+    , gameNumber = 1
+    , entries = initialEntries
+    }
+
+
+initialEntries =
+    [ { id = 1, phrase = "Future-Proof", points = 100, marked = False }
+    , { id = 2, phrase = "Doing Agile", points = 200, marked = False }
+    ]
+
+
+
+-- VIEW
+
+
 playerInfo : String -> Int -> String
 playerInfo name gameNumber =
     name ++ " - Game #" ++ toString gameNumber
@@ -34,15 +54,19 @@ viewFooter =
         ]
 
 
-view : Html msg
-view =
+
+-- view : Html msg
+
+
+view model =
     div [ class "content" ]
         [ viewHeader "BUZZWORD BINGO"
-        , viewPlayer "dd" 1
+        , viewPlayer model.name model.gameNumber
+        , div [ class "debug" ] [ text (toString model) ]
         , viewFooter
         ]
 
 
 main : Html msg
 main =
-    view
+    view initialModel
